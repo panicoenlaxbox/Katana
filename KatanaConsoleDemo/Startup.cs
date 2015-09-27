9962606f34814133807f16fc4755e60d52c05e10
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-09-27 16:07:53 +0200

    Middleware WelcomePage usando el método extensor UseWelcomePage

--- KatanaConsoleDemo/Startup.cs
+++ KatanaConsoleDemo/Startup.cs
@@ -8,8 +8,8 @@ namespace KatanaConsoleDemo
     {
         public void Configuration(IAppBuilder app)
         {
-            var options = new WelcomePageOptions() { Path = new PathString("/welcome") };
-            app.Use<WelcomePageMiddleware>(options);
+            //var options = new WelcomePageOptions() { Path = new PathString("/welcome") };
+            //app.Use<WelcomePageMiddleware>(options);
             app.UseWelcomePage("/welcome");
         }
     }
