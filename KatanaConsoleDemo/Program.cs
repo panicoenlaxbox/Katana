2231f2d8c1e3bbdcecdc7875088492de0bb27ce8
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-09-27 14:14:00 +0200

    Initial commit

---
+++ KatanaConsoleDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace KatanaConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8888"))
+            {
+                Console.WriteLine("Press a key to stop server");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ KatanaConsoleDemo/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Diagnostics;
+using Owin;
+
+namespace KatanaConsoleDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var options = new WelcomePageOptions() { Path = new PathString("/welcome") };
+            app.Use<WelcomePageMiddleware>(options);
+            app.UseWelcomePage("/welcome");
+        }
+    }
+}
