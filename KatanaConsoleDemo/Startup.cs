7fd3e8bd1140b5b332d9ab61bdcece2225873df2
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-09-27 16:23:17 +0200

    Incluso indicar el nombre del método de la clase OwinStatup

--- KatanaConsoleDemo/Startup.cs
+++ KatanaConsoleDemo/Startup.cs
@@ -3,12 +3,12 @@ using Microsoft.Owin;
 using Microsoft.Owin.Diagnostics;
 using Owin;
 
-[assembly: OwinStartup(typeof(MyStartup))]
+[assembly: OwinStartup(typeof(MyStartup), "Configuracion")]
 namespace KatanaConsoleDemo
 {
     public class MyStartup
     {
-        public void Configuration(IAppBuilder app)
+        public void Configuracion(IAppBuilder app)
         {
             //var options = new WelcomePageOptions() { Path = new PathString("/welcome") };
             //app.Use<WelcomePageMiddleware>(options);
