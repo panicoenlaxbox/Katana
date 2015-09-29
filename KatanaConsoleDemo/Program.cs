39954bef3c032a9eed7237f8074c19831a624e11
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-09-29 23:13:48 +0200

    Middlewares personalizados

--- KatanaConsoleDemo/Program.cs
+++ KatanaConsoleDemo/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Owin;
 using Microsoft.Owin.Hosting;
 using Owin;
 
 namespace KatanaConsoleDemo
 {
+    public class PoweredByMiddleware : OwinMiddleware
+    {
+        private readonly string _by;
+
+        public PoweredByMiddleware(OwinMiddleware next, string by) : base(next)
+        {
+            _by = by;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            context.Response.Headers.Append("Powered-by", _by);
+            return Next.Invoke(context);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -11,7 +30,29 @@ namespace KatanaConsoleDemo
             using (WebApp.Start("http://localhost:8888",
                 app =>
                 {
+                    app.Use<PoweredByMiddleware>("tabconsultores");
+                    app.Use(async (context, next) =>
+                    {
+                        var watch = Stopwatch.StartNew();
+                        await next(); // Execute next middleware
+                        watch.Stop(); // Back in the pipeline
+                        await context.Response.WriteAsync("-- Elapsed: " + watch.Elapsed);
+                    });
                     app.UseWelcomePage("/welcome");
+                    app.Use((IOwinContext context, Func<Task> nextModule) =>
+                    {
+                        var startsWithHello = context.Request.Path.Value.StartsWith("/hello");
+                        var name = context.Request.Query.Get("name");
+                        var existsName = !string.IsNullOrWhiteSpace(name);
+                        if (startsWithHello && existsName)
+                        {
+                            var greeting = string.Format("Hello {0}", name);
+                            // Final module, the next middleware will not be executed
+                            return context.Response.WriteAsync(greeting);
+                        }
+                        return nextModule();
+                    });
+
                 }))
             {
                 Console.WriteLine("Press a key to stop server");
